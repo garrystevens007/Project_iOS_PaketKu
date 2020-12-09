7532ad91d03425537a0d4a9813691c56bdf0feab
Garry <garry@Garrys-Mac.local>
2020-12-09 21:42:22 +0700

    Initial Commit

---
+++ binus_ios_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  binus_ios_project
+//
+//  Created by Garry on 09/12/20.
+//  Copyright © 2020 Garry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
