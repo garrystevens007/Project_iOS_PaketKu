e760fe701182c2b61416713e0b08909a6935bd87
garrystevens007 <49149545+garrystevens007@users.noreply.github.com>
2020-12-10 21:45:32 +0700

    Adding some ui and reshuffle

--- binus_ios_project/ViewController.swift
+++ binus_ios_project/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World")
-
-        print("Code here")
-        print("Checker")
-        print("Masuk lagi gan")
-        print("Martin test")
 
       
     }
