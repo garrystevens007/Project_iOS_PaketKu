36292065dcf8916ae1662e8cabac84a70ea332ec
abhisatria <abhiwisesa@gmail.com>
2020-12-12 07:35:11 -0800

    CRU news

--- binus_ios_project/EditViewController.swift
+++ binus_ios_project/EditViewController.swift
@@ -7,24 +7,50 @@
 //
 
 import UIKit
+import Photos
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imgThumbnail: UIImageView!
+    @IBOutlet weak var btnSelectImg: UIButton!
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfDescription: UITextField!
+    var passIndex : IndexPath?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func selectImg(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
     }
-    */
+    @IBAction func saveNews(_ sender: Any) {
+        if passIndex == nil{
+            if let png = self.imgThumbnail.image?.pngData(){
+//                DatabaseHelper.instance.saveNewsInCoreData(at: tfTitle.text!, description: tfDescription.text!, authorEmail: currentUser?.email, authorName: currentUser?.name, index: arr.count, date: nil, imgData: png)
+                DatabaseHelper.instance.saveNewsInCoreData(at: tfTitle.text!, description: tfDescription.text!, authorEmail: currentUser?.email, authorName: "Abhi", index: arr.count, date: nil, imgData: png)
+            }else{
+                print("pap dulu bro")
+            }
+        }
+        else{
+           if let png = self.imgThumbnail.image?.pngData(){
+            DatabaseHelper.instance.updateNews(at: tfTitle.text!, description: tfDescription.text!, index: passIndex!.row, imgData: png)
+           }else{
+               print("pap dulu bro")
+           }
+        }
+        performSegue(withIdentifier: "saveNews", sender: self)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let userPickedImage = info[.editedImage] as? UIImage else { return }
+            imgThumbnail.image = userPickedImage
+            picker.dismiss(animated: true)
+    }
 
 }
